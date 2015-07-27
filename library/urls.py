9bf8186d793d80a2e4f572bc7838a73ff0888cb3
re4lfl0w <re4lfl0w@gmail.com>
2015-07-27 20:13:31 +0900

    Add first functional tests

--- library/urls.py
+++ library/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'eag_lib.views.home', name='home'),
+    url(r'^$', 'eag_lib.views.home', name='home'),
 )
