795ea2c15a8f3b20dec8929f22b46a177bc46d63
re4lfl0w <re4lfl0w@gmail.com>
2015-08-04 15:34:41 +0900

    Add help_text in model

--- check_app_version/models.py
+++ check_app_version/models.py
@@ -1,17 +1,17 @@
-import os
+# -*- coding: utf-8 -*-
 from django.db import models
 
 
 class AppVersion(models.Model):
-    app_id = models.CharField(max_length=20, null=False, blank=False)
-    app_version = models.CharField(max_length=10, null=False, blank=False)
+    app_id = models.CharField(max_length=20, null=False, blank=False, help_text='MobiusPushTaesan001 로 고정합니다. App id가 달라지면 안됩니다.')
+    app_version = models.CharField(max_length=10, null=False, blank=False, help_text='App의 버전을 입력합니다. 모바일 어플은 이 정보로 새로운 App인지 비교하기 때문에 중요합니다.')
     # file_name = models.CharField(max_length=50, null=False, blank=False)
     # file_path = models.CharField(max_length=200, null=False, blank=False)
-    apk_file = models.FileField(upload_to='apk/')
-    apk_size = models.IntegerField(default=0)
+    apk_file = models.FileField(upload_to='apk/', help_text='apk 파일만 올립니다.')
+    apk_size = models.IntegerField(default=0, help_text='자동 계산 됩니다. 입력 생략')
     # ipa_file = models.FileField(upload_to='ipa/')
     # file_size = models.BigIntegerField(null=True)
-    download_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    download_cnt = models.BigIntegerField(default=0, null=False, blank=False, help_text='자동 계산 됩니다. 입력 생략')
     register_date = models.DateTimeField(auto_now_add=True, auto_now=True)
 
     def save(self, *args, **kwargs):
